10291c15ff60abf42e93b63a6884f05bde1ee4bb
ruparathna <piyumi.ishara@my.sliit.lk>
2018-08-03 15:16:29 +0530

    family budget plan

---
+++ app/src/main/java/com/example/piyu/myapplication123/familybudget.java
@@ -0,0 +1,13 @@
+package com.example.piyu.myapplication123;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class familybudget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_familybudget);
+    }
+}
